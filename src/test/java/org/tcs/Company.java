0995780f2fc1c4e3c7ef3b954caced7a6db711a0
alagu perumal <perumalalagu79@gmail.com>
2026-04-22 18:21:25 +0530

    Added methods and class company from Alaguperumal

---
+++ src/test/java/org/tcs/Company.java
@@ -0,0 +1,12 @@
+package org.tcs;
+
+public class Company {
+	private void Javaclass() 
+	{
+		System.out.println("Java Class");
+	}
+	private void Pythonclass() 
+	{
+		System.out.println("Python Class");
+	}
+}
